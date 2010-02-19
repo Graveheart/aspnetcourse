42d6844bea56137273e2e9bb2b99364b044f793d
svetlin <svetlin@nakov.com>
2010-02-19 17:37:25 +0000

    Added console I/O materials

---
+++ materials/2 & 3. C# Language Overview - Part I and II/Console-Demos/ReadingNumbers/ReadingNumbers.cs
@@ -0,0 +1,23 @@
+using System;
+
+class ReadingNumbers
+{
+    static void Main()
+    {
+        Console.Write("a = ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("b = ");
+        int b = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("{0} + {1} = {2}",
+            a, b, a + b);
+        Console.WriteLine("{0} * {1} = {2}",
+            a, b, a * b);
+
+        Console.Write("f = ");
+        float f = float.Parse(Console.ReadLine());
+        Console.WriteLine("{0} * {1} / {2} = {3}",
+            a, b, f, a * b / f);
+    }
+}
